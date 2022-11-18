8083977009d54dc79000494fcd0eacb20d98bc2c
kromatic97 <rogiromero@gmail.com>
2022-11-18 16:25:08 -0300

    modifique el css del slider

--- src/views/NavBar.jsx
+++ src/views/NavBar.jsx
@@ -4,49 +4,52 @@ import logo from '../assets/images/logo.png'
 
 const NavBar = () => {
   return (
-<div className='container'>
-    
-<nav className="navbar navbar-expand-lg navbar-light bg-light navbarprinc">
-  <div className="container-fluid">
+    <div className='container'>
 
-<div className='container_logo'>
-    <Link to= '/'> 
-      <img className="logo" src={logo} alt="imagen"/>
-    </Link>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light navbarprinc">
+        <div className="container-fluid">
 
-</div>
+          <div className='container_logo'>
+            <Link to='/'>
+              <img className="logo" src={logo} alt="imagen" />
+            </Link>
 
+          </div>
 
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to='/'>Home</Link>
+              </li>
 
-        <li className="nav-item">
-          <Link className="nav-link" to='/'>Home</Link>
-        </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/courses'>Courses</Link>
+              </li>
 
-        <li className="nav-item">
-          <Link className="nav-link" to='/courses'>Courses</Link>
-        </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/categories'>Categories</Link>
+              </li>
 
-        <li className="nav-item">
-          <Link className="nav-link" to='/categories'>Categories</Link>
-        </li>
+              <li className="nav-item">
+                <Link className="nav-link" to='/sign'>Sign</Link>
+              </li>
 
-        <li className="nav-item">
-          <Link className="nav-link" to='/sign'>Sign</Link>
-        </li>
-      </ul>
-      <div className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </div>
-    </div>
-  </div>
-</nav>
+              <li className="nav-item">
+                <Link className="nav-link" to='/sign'>Black Friday</Link>
+              </li>
+
+            </ul>
+            <div className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
